c9e7779e2955ddd5c2d7095f3ba12b9eaa163c69
Bradley Norris <risebaxelrod@gmail.com>
2019-01-25 11:17:24 -0800

    google login up and running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: "AIzaSyC7-T4wi6AWcO04Kta1oTo0lkhXwWXQ1XI",
+  authDomain: "owldoit-54a72.firebaseapp.com",
+  databaseURL: "https://owldoit-54a72.firebaseio.com",
+  projectId: "owldoit-54a72",
+  storageBucket: "owldoit-54a72.appspot.com",
+  messagingSenderId: "1004497354491"
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +25,10 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
