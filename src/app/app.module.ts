e2d4ca2eb8098ff21e203f6ddaaafcb47d4ab10d
Elizabeth <elizabethallen36@gmail.com>
2019-01-28 18:07:38 -0800

    added customer profile files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Profile1Component } from './profile1/profile1.component';
+import { Profile2Component } from './profile2/profile2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Profile1Component
+    Profile1Component,
+    Profile2Component
   ],
   imports: [
     BrowserModule,
