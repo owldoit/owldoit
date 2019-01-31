aebc9afa605955a1ac8da2c2de6e24a7dafefc2a
Bradley Norris <risebaxelrod@gmail.com>
2019-01-30 18:52:52 -0800

    moved login module to the login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProjectFormComponent } from './projects/project-form/project-form.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-	{ path: 'project', component: ProjectFormComponent }
+  { path: 'project', component: ProjectFormComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
