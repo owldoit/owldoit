8e1cd3281614f377eb3bc961381394d7f18a7f8a
Elizabeth <elizabethallen36@gmail.com>
2019-01-30 19:54:57 -0800

    resolved yet?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { Profile1Component } from './profile1/profile1.component';
 import { Profile2Component } from './profile2/profile2.component';
-
 import { ProjectFormComponent } from './projects/project-form/project-form.component';
 
 
